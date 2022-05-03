57160ae5842dd009e22ad581eed0c7f901567896
GesMon23 <100744918+GesMon23@users.noreply.github.com>
2022-05-02 20:37:18 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//DESPLEGAR ENTEROS ENTRE 50 Y 80.
+#include <iostream>
+using namespace std;
+int main(int argc, char** argv){
+	int Num;
+	Num=50;	
+	while(Num<=80){
+		printf("%d\n", Num);
+		Num++;
+	}
+	system("pause");
+}
